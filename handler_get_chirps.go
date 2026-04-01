cdaaaf3fde37d65a763ad73185b753dc41afb4ca
BeagleBasset <deneslajoskrisztian@gmail.com>
2026-04-01 07:13:30 +0200

    finish the course

--- handler_get_chirps.go
+++ handler_get_chirps.go
@@ -2,26 +2,71 @@ package main
 
 import (
 	"net/http"
+	"sort"
+
+	"github.com/google/uuid"
 )
 
 func (cfg *apiConfig) handlerGetAllChirps(w http.ResponseWriter, r *http.Request) {
-	dbChirps, err := cfg.dbQueries.GetAllChirps(r.Context())
-	if err != nil {
-		respondWithError(w, 500, "Hiba a chirp-ek lekérdezése közben", err)	
-		return
+		sortParam := r.URL.Query().Get("sort")
+	if sortParam == "" {
+		sortParam = "asc"
 	}
 
-	resps := []Chirp{}
-	for _, dbChirp := range dbChirps {
-		resp := Chirp{
-			ID:			dbChirp.ID,
-			CreatedAt: 	dbChirp.CreatedAt,
-			UpdatedAt: 	dbChirp.UpdatedAt,
-			Body: 		dbChirp.Body,
-			UserID:		dbChirp.UserID,
+	authorIDString := r.URL.Query().Get("author_id")
+	if authorIDString != "" {
+		authorID, err := uuid.Parse(authorIDString)
+		if err != nil {
+			respondWithError(w, 400, "Error in parsing:", err)
+			return
 		}
-		resps = append(resps, resp)
-	}
 
-	respondWithJSON(w, 200, resps)
+		dbChirps, err := cfg.dbQueries.GetChirpFromUser(r.Context(), authorID)
+		if err != nil {
+			respondWithError(w, 500, "Error in query:", err)
+			return
+		}
+
+		resps := []Chirp{}
+		for _, dbChirp := range dbChirps {
+			resp := Chirp{
+				ID:			dbChirp.ID,
+				CreatedAt: 	dbChirp.CreatedAt,
+				UpdatedAt: 	dbChirp.UpdatedAt,
+				Body: 		dbChirp.Body,
+				UserID:		dbChirp.UserID,
+			}
+			resps = append(resps, resp)
+		}
+
+		respondWithJSON(w, 200, resps)
+
+	} else {
+		dbChirps, err := cfg.dbQueries.GetAllChirps(r.Context())
+		if err != nil {
+			respondWithError(w, 500, "Hiba a chirp-ek lekérdezése közben", err)	
+			return
+		}
+
+		resps := []Chirp{}
+		for _, dbChirp := range dbChirps {
+			resp := Chirp{
+				ID:			dbChirp.ID,
+				CreatedAt: 	dbChirp.CreatedAt,
+				UpdatedAt: 	dbChirp.UpdatedAt,
+				Body: 		dbChirp.Body,
+				UserID:		dbChirp.UserID,
+			}
+			resps = append(resps, resp)
+		}
+
+		sort.Slice(resps, func(i, j int) bool {
+			if sortParam == "desc" {
+				return resps[i].CreatedAt.After(resps[j].CreatedAt)
+			}
+			// default asc
+			return resps[i].CreatedAt.Before(resps[j].CreatedAt)
+		})
+		respondWithJSON(w, 200, resps)
+	}
 }
